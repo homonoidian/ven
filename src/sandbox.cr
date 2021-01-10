a12c43219d38e7aa0ef28e496cd2140422339b77
Alexey Yurchenko <homonoidian@yandex.ru>
2021-01-10 12:59:38 +0300

    'sandbox.cr': uncomment & modify so it benchmarks 'examples/lexer.cr'

--- src/sandbox.cr
+++ src/sandbox.cr
@@ -2,10 +2,10 @@ require "./ven/**"
 
 require "benchmark"
 
-# lexer = File.read("examples/lexer.ven")
+lexer = File.read("examples/lexer.ven")
 
-# Benchmark.ips do |x|
-#   x.report("Ven::Read") do
-#     Ven::Read.from("<sandbox>", lexer)
-#   end
-# end
+ Benchmark.ips do |x|
+   x.report("Ven.read") do
+     Ven.read("<sandbox>", lexer)
+   end
+ end
