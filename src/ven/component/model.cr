282bfe9f56228e7ef32f20751f49390146d7b8ed
Alexey Yurchenko <homonoidian@yandex.ru>
2021-01-27 16:54:42 +0300

    'component/model': rewrite some of the comments. Rename `Str.rawen!` to `Str.unescape`

--- src/ven/component/model.cr
+++ src/ven/component/model.cr
@@ -1,17 +1,18 @@
 require "big"
 
 module Ven::Component
-  # An exception raised when a conversion (cast) error occurs
-  # (e.g., failed converting (casting) string to number.)
+  # An exception raised when there is a conversion (cast)
+  # error (e.g., in parsing a string into a number.)
   class ModelCastException < Exception
   end
 
-  # # The base type of Ven's value system and, therefore, the
-  # root of Ven's type system. Unifies `MClass` and `MStruct`.
+  # The base type of Ven's value system and, therefore, the
+  # root of Ven's type system. A union of `MClass` and `MStruct`.
   alias Model = MClass | MStruct
 
   # :nodoc:
   macro model_template?
+    # The fields this model gives access to.
     @@FIELDS = {} of String => Model
 
     # Converts (casts) this model into an `MNumber`.
@@ -29,7 +30,8 @@ module Ven::Component
       MVector.new([self.as(Model)])
     end
 
-    # Returns a field's value for this model.
+    # Returns a field's value for this model (or nil if the
+    # field of interest does not exist.)
     def field(name : String) : Model?
       @@FIELDS[name]?
     end
@@ -40,11 +42,13 @@ module Ven::Component
     end
   end
 
-  # A kind of `Model` for value objects (represented by Crystal `struct`).
+  # A kind of `Model`s that can be safely represented as a
+  # Crystal struct.
   abstract struct MStruct
     Component.model_template?
   end
 
+  # A Ven wrapper for a particular Crystal type *T*.
   abstract struct MValue(T) < MStruct
     property value
 
@@ -66,15 +70,79 @@ module Ven::Component
   # :nodoc:
   alias Vec = MVector
 
-  # Ven boolean. Emboxes a `Bool`.
+  # A Ven boolean.
   struct MBool < MValue(Bool)
     def to_num
       Num.new(@value ? 1 : 0)
     end
   end
 
-  # Ven number. Emboxes a `BigRational`.
-  struct MNumber < MStruct
+  # A Ven string.
+  struct MString < MValue(String)
+    # A hash of escaped escape sequences and what they should
+    # evaluate to.
+    SEQUENCES = {
+      "\\n" => "\n",
+      "\\t" => "\t",
+      "\\r" => "\r",
+      "\\\"" => "\"",
+      "\\\\" => "\\"
+    }
+
+    # Evaluates the escape sequences in the *operand* String.
+    def self.unescape(operand : String)
+      SEQUENCES.each do |escape, raw|
+        operand = operand.gsub(escape, raw)
+      end
+
+      operand
+    end
+
+    # Returns the length of this string if *parse* is false,
+    # which it is by default; otherwise, if it is true, returns
+    # this string parsed into an `MNumber`.
+    def to_num(parse = false)
+      Num.new(parse ? @value : @value.size)
+    rescue InvalidBigDecimalException
+      raise ModelCastException.new("#{self} is not a base-10 number")
+    end
+
+    def to_str
+      self
+    end
+
+    def to_s(io)
+      @value.dump(io)
+    end
+  end
+
+  # A Ven regex.
+  struct MRegex < MStruct
+    property value
+
+    def initialize(
+      @value : Regex,
+      @string : String = @value.to_s)
+    end
+
+    def to_s(io)
+      io << "`" << @string << "`"
+    end
+
+    def to_str
+      Str.new(@string)
+    end
+  end
+
+  # A kind of `Model`s that can be safely represented as
+  # a Crystal class. Often these are `Model`s that have
+  # no particular value.
+  abstract class MClass
+    Component.model_template?
+  end
+
+  # A Ven number, implemented with caching in mind.
+  class MNumber < MClass
     property value : BigRational
 
     CACHE = {} of String => BigRational
@@ -99,71 +167,7 @@ module Ven::Component
     end
   end
 
-  # Ven string. Emboxes a `String`.
-  struct MString < MValue(String)
-    SEQUENCES = {
-      "\\n" => "\n",
-      "\\t" => "\t",
-      "\\r" => "\r",
-      "\\\"" => "\"",
-      "\\\\" => "\\"
-    }
-
-    # Evaluate the escape sequences in the *operand* String.
-    def self.rawen!(operand : String)
-      SEQUENCES.each do |escape, raw|
-        operand = operand.gsub(escape, raw)
-      end
-
-      operand
-    end
-
-    # Parse this string into a number.
-    def parse_num
-      Num.new(@value)
-    rescue InvalidBigDecimalException
-      raise ModelCastException.new("#{self} is not a base-10 number")
-    end
-
-    # Return the length of this string.
-    def to_num
-      Num.new(@value.size)
-    end
-
-    def to_str
-      self
-    end
-
-    def to_s(io)
-      @value.dump(io)
-    end
-  end
-
-  # Ven regex. Emboxes a `Regex`.
-  struct MRegex < MStruct
-    property value
-
-    def initialize(
-      @value : Regex,
-      @string : String = @value.to_s)
-    end
-
-    def to_s(io)
-      io << "`" << @string << "`"
-    end
-
-    def to_str
-      Str.new(@string)
-    end
-  end
-
-  # A kind of `Model` for reference entities and entities
-  # without a value (represented by Crystal `class`).
-  abstract class MClass
-    Component.model_template?
-  end
-
-  # Ven vector. Emboxes an Array of Models.
+  # A Ven vector, essentially, an Array of `Model`s.
   class MVector < MClass
     property value
 
@@ -189,8 +193,8 @@ module Ven::Component
     end
   end
 
-  # Ven type. Emboxes a typename and the model class this
-  # type represents.
+  # A Ven type, which emboxes the name of the type and the
+  # `Model` this type should then represent.
   class MType < MClass
     getter name, type
 
@@ -201,10 +205,10 @@ module Ven::Component
       @type : MClass.class | MStruct.class)
     end
 
-    # Returns whether this type's name and type are equal to
-    # the *other*'s name and type.
+    # Returns whether this type's name and model are equal to
+    # the *other*'s name and model.
     def ==(other : MType)
-      name == other.name && type == other.type
+      @name == other.name && @type == other.type
     end
 
     def to_s(io)
@@ -212,24 +216,29 @@ module Ven::Component
     end
   end
 
-  # A type that matches anything. Has no value and cannot be
-  # instantiated from Crystal (nor can it be from Ven).
+  # A type that matches anything. It has no value and cannot
+  # be directly interacted with in Crystal nor in Ven.
   abstract class MAny < MClass
   end
 
+  # A tuple that consists of parameter's name and parameter's
+  # `MType`.
   alias TypedParameter = {String, MType}
 
-  # An umbrella `function` type.
+  # An umbrella `function` type, and a parent for all kinds
+  # of functions Ven has.
   abstract class MFunction < MClass
     def callable?
       true
     end
   end
 
-  # A variant of a generic function. Emboxes this variation's
-  # *name*, *body*, *tag* and constraints. All functions are
-  # constrained, but `any` restriction is used by default
-  # (it matches everything).
+  # A concrete implementation of a generic function. It has,
+  # amongst others, a *name*, a *body*, a *tag*, and a set of
+  # *constraints* which define the very identity of any concrete
+  # function. All concrete functions are constrained, but the `any`
+  # constraint is used by default, allowing unconstraintness
+  # to be a thing.
   class MConcreteFunction < MFunction
     getter tag : QTag,
       name : String,
@@ -268,9 +277,8 @@ module Ven::Component
     end
   end
 
-  # An abstract callable entity that manages a list of variants
-  # (see `MConcreteFunction`). The model Emboxes the name of
-  # this generic and the list of variants.
+  # An abstract callable entity that supervises a list of
+  # implementations (variants) under the same name.
   class MGenericFunction < MFunction
     getter name
 
@@ -281,7 +289,8 @@ module Ven::Component
       @@FIELDS["name"] = Str.new(@name)
     end
 
-    # Adds a *variant*. Replaces identical variants, if found any.
+    # Adds a *variant*. Replaces identical variants, if found
+    # any.
     def add(variant : MConcreteFunction)
       target = variant.general ? @general : @strict
 
@@ -297,13 +306,17 @@ module Ven::Component
       variant
     end
 
-    # Returns the strict and general variants this generic has
-    # under one Array.
+    # Returns an Array of the strict and general variants of
+    # this generic.
     def variants
       @strict + @general
     end
 
     def field(name)
+      # These are dynamic fields; their values change over
+      # time. Thus, `model_template`'s static @@FIELD system
+      # does not work for us so well anymore.
+
       case name
       when "variants"
         Vec.new(variants.map { |c| c.as(Model) })
@@ -319,10 +332,7 @@ module Ven::Component
     end
   end
 
-  # One of the bridges from Crystal to Ven. The model emboxes
-  # the name of this builtin and a Crystal Proc. This Proc
-  # takes two arguments: the first for an instance of `Machine`,
-  # the other for an Array of arguments; and returns a Model.
+  # A bridge from Crystal to Ven.
   class MBuiltinFunction < MFunction
     getter name, block
 
